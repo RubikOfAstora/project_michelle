5100469263da8cf71004fa08a7fb2a4b9ebc0c0e
Kevin Schulthess <kevin_schulthess@bluewin.ch>
2021-07-12 20:00:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app     = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+console.log(`Running at Port ${port}`);
+
+app.get('/index.html', (req, res) => {
+    res.send("Contract for the dive course");
+})
